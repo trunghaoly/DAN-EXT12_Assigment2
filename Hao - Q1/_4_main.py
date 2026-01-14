968ad113995f820ba7a40365f14ac94dcde0d017
Lee Hao <trunghao.ly@gmail.com>
2026-01-14 13:52:21 +0930

    update

--- Hao - Q1/_4_main.py
+++ Hao - Q1/_4_main.py
@@ -1,25 +1,7 @@
 # Import custom modules
 from _1_encrypt import *
 from _2_decrypt import *
-
-def compare():
-    """
-    Verifies if the decrypted text matches the original raw text.
-    """
-
-    # Open files for verification
-    with    open('raw_text.txt','r',encoding='utf-8') as rawtext, \
-            open('decrypted_text.txt','r',encoding='utf-8') as decrypted:
-    
-        # Read content
-        rawdata = rawtext.read()
-        decrypteddata = decrypted.read()
-    
-        # Compare and print result
-        if rawdata == decrypteddata:
-            print('SUCCESS: The decrypted text matches the original.')
-        else:
-            print('FAILURE: The content does not match.')
+from _3_verify import *
 
 def main():
 
